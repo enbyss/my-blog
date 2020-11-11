0f664fa9b248aedd8c0de1c1610cefa501af4170
Calmynt <mctech26@gmail.com>
2020-11-11 15:51:37 +0100

    Revamped styling and colour scheme.

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
     <header class="transition duration-200 w-full bg-white border-b-2 dark:border-blue-700
-                  flex justify-center h-16 items-center bg-white border-gray-200
-                  dark:bg-blue-900 dark:text-white">
+                  flex justify-center h-16 items-center border-gray-200
+                  dark:text-white">
       <NuxtLink :class="{'ml-5' : mobile}" to="/">
-        <img src="~/assets/images/icon.png" class="h-10 mr-2 shadow-2xl rounded-full" style="box-shadow: 0 0 10px #3e8eff;"/>
+        <img src="~/assets/images/icon.png" class="main-logo h-10 hover:h-12 mr-2 shadow-2xl rounded-full" style="box-shadow: 0 0 10px #3e8eff;"/>
       </NuxtLink>
 
       <input
           placeholder="Search..."
           :class="{'ml-auto mr-auto' : mobile}"
           class="transition duration-200 w-48 sm:w-72 md:w-96 bg-gray-300 h-10 rounded-xl px-5 ml-5 mr-5
-                dark:bg-blue-700 dark:text-blue-200 dark:placeholder-blue-300 border-transparent border-2
-                focus:border-blue-400 dark-focus:border-blue-700"
+                dark:bg-gray-900 dark:text-blue-200 dark:placeholder-blue-300 border-transparent border-2
+                focus:border-blue-400 dark-focus:border-blue-700 placeholder-blue-700"
       />
 
       <!-- Links to other pages. -->
       <div v-if="!mobile">
-        <NuxtLink class="header-link hover:bg-blue-300 hover:text-blue-800" to="/content">Blogs</NuxtLink>
+        <NuxtLink class="header-link hover:bg-blue-300 hover:text-blue-800" to="/content">Content</NuxtLink>
         <NuxtLink class="header-link hover:bg-red-300 hover:text-red-800" to="/about">About</NuxtLink>
         <NuxtLink class="header-link hover:bg-yellow-300 hover:text-yellow-800" to="/contact">Contact</NuxtLink>
       </div>
 
       <!-- Buttons for dark mode and potentially the hamburger menu. -->
       <div :class="{'ml-auto mr-5' : mobile}">
-        <button class="ml-3 rounded-full text-xl" @click="toggleDarkMode()">
+        <button class="transition duration-200 dark-hover:bg-gray-800 hover:bg-gray-200 w-10 h-10 ml-3 rounded-full text-xl" @click="toggleDarkMode()">
           <font-awesome-icon v-if="!darkMode" :icon="['fas', 'sun']" />
           <font-awesome-icon v-if="darkMode" :icon="['fas', 'moon']" />
         </button>
@@ -38,14 +38,14 @@
 
     <!-- Menu on mobile. -->
     <transition name="fade">
-      <ul v-if="mobile & showMenu" class="transform origin-top absolute w-full bg-gray-100 text-gray-700 dark:bg-megadark dark:text-white font-bold text-2xl">
-        <li class="bg-gray-100 dark:bg-megadark border-b-2 border-blue-200 dark:border-blue-800 h-12 flex items-center">
-          <NuxtLink class="block w-full px-5" to="/content">Blogs</NuxtLink>
+      <ul v-if="mobile & showMenu" class="burger-menu-list transform origin-top absolute w-full bg-gray-100 text-gray-700 dark:bg-megadark dark:text-white font-bold text-2xl">
+        <li>
+          <NuxtLink class="block w-full px-5" to="/content">Content</NuxtLink>
         </li>
-        <li class="bg-gray-100 dark:bg-megadark border-b-2 border-blue-200 dark:border-blue-800 h-12 flex items-center">
+        <li>
           <NuxtLink class="block w-full px-5" to="/about">About</NuxtLink>
         </li>
-        <li class="bg-gray-100 dark:bg-megadark border-b-2 border-blue-200 dark:border-blue-800 h-12 flex items-center">
+        <li>
           <NuxtLink class="block w-full px-5" to="/contact">Contact</NuxtLink>
         </li>
       </ul>
@@ -109,6 +109,48 @@ export default {
   }
   .fade-enter-to, .fade-leave {
     @apply translate-x-0 h-auto;
+  }
+
+  .main-logo {
+    transition: all 0.2s ease-in-out;
+  }
+
+  .main-logo:hover {
+    filter: hue-rotate(45deg);
+    transform: scale(1.2) rotate(180deg);
+  }
+
+  .burger-menu-list {
+    li {
+      @apply border-t-2 border-b-2 border-gray-200 h-12 flex items-center transition transition-all duration-200 text-gray-800;
+      background: linear-gradient(90deg, rgb(191, 168, 255), rgb(255, 169, 209));
+
+      &:hover {
+        @apply border-b-4 border-t-4 border-pink-600;
+      }
+    }
+  }
+
+  // Dark mode theming
+  .mode-dark {
+    header {
+      background: linear-gradient(90deg, rgb(68, 0, 255), rgb(255, 0, 119))
+    }
+    .burger-menu-list {
+      li {
+        @apply bg-megadark border-blue-800 text-white;
+        background: linear-gradient(90deg, rgb(39, 0, 145), rgb(128, 0, 60));
+
+        &:hover {
+          @apply border-b-4 border-t-4 border-pink-600;
+        }
+      }
+    }
+  }
+
+  // Light mode theming
+  header {
+    background: linear-gradient(90deg, rgb(156, 120, 255), rgb(255, 120, 183))
   }
 </style>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,21 @@
 <script>
 export default {
   mounted() {
-    document.body.classList.add("bg-white", "dark:bg-ultradark", "transition", "duration-200");
+    document.body.classList.add("bg-white", "transition", "duration-200");
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
 
 h1, h2, h3, h4, p, a, figcaption, ul, li, table > * {
   font-family: 'Zilla Slab';
+}
+
+.mode-dark {
+  body {
+    background: rgb(14, 0, 36);
+  }
 }
 </style>
