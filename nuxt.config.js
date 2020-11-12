b3dff4a3946d8bb150bb4cffe01f00ccf2f82da0
Calmynt <mctech26@gmail.com>
2020-11-12 18:26:51 +0100

    Updated dynamic routing further

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
       const { $content } = require('@nuxt/content');
       const files = await $content({ deep: true }).only(['path']).fetch();
 
-      return files.map(file => file.path === '/index' ? '/' : file.path);
+      return files.map(file => file.path === '/index' ? '/content/' : '/content' + file.path);
     }
   },
 
