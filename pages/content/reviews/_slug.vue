684c3bf11c7aa7d57a142ec8288760e3e47f0b0e
Calmynt <mctech26@gmail.com>
2020-11-25 01:09:25 +0100

    Removed comment section.

--- pages/content/reviews/_slug.vue
+++ pages/content/reviews/_slug.vue
@@ -2,14 +2,12 @@
   <div>
     <BaseDocument :document="review" />
     <ReviewScore :score="review.rating" :summary="review.ratingSummary" />
-    <SendComment :slug="review.slug" class="mx-auto w-10/12 lg:w-7/12" />
   </div>
 </template>
 
 <script>
 import BaseDocument from "@/components/BaseDocument.vue";
 import ReviewScore from "@/components/ReviewScore.vue";
-import SendComment from "@/components/SendComment.vue";
 
 export default {
   async asyncData({ $content, params }) {
@@ -20,7 +18,6 @@ export default {
   components : {
     BaseDocument,
     ReviewScore,
-    SendComment
   },
   head() {
     return {
